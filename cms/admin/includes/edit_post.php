4ee8acb8d422545d4f51e467eeb73821ee00a46f
samir823 <samirkafa@gmail.com>
2018-03-05 00:50:35 +0100

    push database

--- cms/admin/includes/edit_post.php
+++ cms/admin/includes/edit_post.php
@@ -6,7 +6,7 @@ $the_post_id = $_GET['p_id'];
     
     
 }
-    $query = "SELECT * FROM posts";
+    $query = "SELECT * FROM posts WHERE post_id = {$the_post_id}";
     $select_post_by_id = mysqli_query($connection, $query);
 
 
@@ -23,6 +23,43 @@ $the_post_id = $_GET['p_id'];
         $post_date =$row['post_date'];
         }
 
+    if (isset($_POST['update_post'])){
+        $post_id =$row['post_id'];
+        $post_author =$row['post_author'];
+        $post_title =$row['post_title'];
+        $post_category_id =$row['post_category_id'];
+        $post_status =$row['post_status'];
+        $post_image =$row['post_image'];
+        $post_content =$row['post_content'];
+        $post_tags =$row['post_tags'];
+        $post_comment_count =$row['post_comment_count'];
+        $post_date =$row['post_date'];    
+        $post_image_temp = $_FILES['image']['tmp_name'];
+         echo $post_category_id;
+        /*
+        
+        move_uploaded_file($post_image_temp, "../admin/images/$post_image");
+        
+        
+        $query = "UPDATE posts SET ";
+        $query .= "post_title = '{$post_title}', ";
+        $query .= "post_category_id = '{$post_category_id}', ";
+        $query .= "post_date = now(), ";
+        $query .= "post_author = '{$post_author}', ";
+        $query .= "post_status = '{$post_status}', ";
+        $query .= "post_tags = '{$post_tags}', ";
+        $query .= "post_content = '{$post_content}', ";
+        $query .= "post_image = '{$post_image}' ";
+        $query .= "WHERE post_id = '{$the_post_id}' ";
+
+
+$update_post = mysqli_query($connection,$query);
+        confirm($update_post);
+*/
+        
+    }
+
+
 ?>
    
    <form action="" method="post" enctype="multipart/form-data">
@@ -31,9 +68,32 @@ $the_post_id = $_GET['p_id'];
        <input value="<?php echo $post_title;?>" type="text" class="form-control" name="title">
     </div>
      <div class="form-group">
-              <label for="post_category">Post Category Id</label>
-       <input value="<?php echo $post_category_id;?>" type="text" class="form-control" name="post_category_id">
-    </div>
+              
+         <select name="post_category_id" id="">
+             
+             <?php
+             $query = "SELECT * FROM categories";
+            $select_categories = mysqli_query($connection, $query);
+
+             
+             confirm($select_categories);
+             
+            while($row = mysqli_fetch_assoc($select_categories)){
+            $cat_id =$row['cat_id'];
+            $cat_title =$row['cat_title'];
+                                    
+                echo "<option value = '$cat_id'>{$cat_title}</option>";
+                
+                
+            }
+                                                ?>
+             
+             
+             
+         </select>  
+              
+       </div>
+    
      <div class="form-group">
               <label for="title">Post Author</label>
        <input value="<?php echo $post_author;?>" type="text" class="form-control" name="author">
@@ -57,7 +117,7 @@ $the_post_id = $_GET['p_id'];
          </textarea>
     </div>
     <div class = "form-group">
-        <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
+        <input class="btn btn-primary" type="submit" name="update_post" value="Update Post">
     </div>
     
     
---
+++ cms/cms.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 05, 2018 at 12:46 AM
+-- Server version: 5.7.21-0ubuntu0.16.04.1
+-- PHP Version: 7.0.22-0ubuntu0.16.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `cat_id` int(3) NOT NULL,
+  `cat_title` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`cat_id`, `cat_title`) VALUES
+(1, 'Bootstrap'),
+(2, 'Javascript'),
+(3, 'Java'),
+(4, 'PHP');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `post_id` int(3) NOT NULL,
+  `post_category_id` int(3) NOT NULL,
+  `post_title` varchar(255) NOT NULL,
+  `post_author` varchar(255) NOT NULL,
+  `post_date` date NOT NULL,
+  `post_image` text NOT NULL,
+  `post_content` text NOT NULL,
+  `post_tags` varchar(255) NOT NULL,
+  `post_comment_count` varchar(255) NOT NULL,
+  `post_status` varchar(255) NOT NULL DEFAULT 'draft'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`post_id`, `post_category_id`, `post_title`, `post_author`, `post_date`, `post_image`, `post_content`, `post_tags`, `post_comment_count`, `post_status`) VALUES
+(2, 2, 'Javascript Course Post', 'Belinda', '2018-01-25', 'javascript.png', 'Wow I was very lucky to participate on this course.', 'javascript, belinda', '', 'draft'),
+(4, 23, 'Test 1', 'samir823', '2018-01-28', 'php.png', 'This is a first post\r\n', 'fist post', '4', 'Publish');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`post_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `cat_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `post_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
